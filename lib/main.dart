c66d1e1ba094d233fe6e110f2a8b461c50cbf141
AntoineLetailleur <ant.letailleur@gmail.com>
2024-02-02 11:25:47 +0100

    hola

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Chez',
+      title: 'Chez mémé',
       theme: ThemeData(
 
         primarySwatch: Colors.green,
